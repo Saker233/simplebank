24fc8927ef757012d8f949d23b7bab24d32023ec
Mohamed Saker <mohamedsaker233@gmail.com>
2026-07-03 03:16:59 +0300

    Adding a helper function addMoney so we prevent boilerplate code

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -112,8 +112,6 @@ func TestTransferTx(t *testing.T) {
 	require.Equal(t, acc2.Balance+int64(n)*amount, updateAccount2.Balance)
 }
 
-
-
 func TestTransferTxDeadlock(t *testing.T) {
 	store := NewStore(testDB)
 
@@ -130,80 +128,27 @@ func TestTransferTxDeadlock(t *testing.T) {
 	for i := 0; i < n; i++ {
 		fromAccountID := acc1.ID
 		toAccountID := acc2.ID
+
+		if i%2 == 1 {
+			fromAccountID = acc2.ID
+			toAccountID = acc1.ID
+		}
+
 		go func() {
 			ctx := context.Background()
-			result, err := store.TransferTx(ctx, TransferTxParams{
-				FromAccountID: acc1.ID,
-				ToAccountID:   acc2.ID,
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
 				Amount:        amount,
 			})
 			errs <- err
-			results <- result
 		}()
 	}
 	// check results
-	existed := make(map[int]bool)
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
 
-		result := <-results
-		require.NotEmpty(t, result)
-
-		// check transfer
-		transfer := result.Transfer
-
-		require.NotEmpty(t, transfer)
-		require.Equal(t, acc1.ID, transfer.FromAccountID)
-		require.Equal(t, acc2.ID, transfer.ToAccountID)
-		require.Equal(t, amount, transfer.Amount)
-		require.NotZero(t, transfer.ID)
-		require.NotZero(t, transfer.CreatedAt)
-
-		_, err = store.GetTransfer(context.Background(), transfer.ID)
-		require.NoError(t, err)
-
-		// check entries
-		fromEntry := result.FromEntry
-		require.NotEmpty(t, fromEntry)
-		require.Equal(t, acc1.ID, fromEntry.AccountID)
-		require.Equal(t, -amount, fromEntry.Amount)
-		require.NotZero(t, fromEntry.CreatedAt)
-
-		_, err = store.GetEntry(context.Background(), fromEntry.ID)
-		require.NoError(t, err)
-
-		toEntry := result.ToEntry
-		require.NotEmpty(t, toEntry)
-		require.Equal(t, acc2.ID, toEntry.AccountID)
-		require.Equal(t, amount, toEntry.Amount)
-		require.NotZero(t, toEntry.CreatedAt)
-		require.NotZero(t, toEntry.ID)
-
-		_, err = store.GetEntry(context.Background(), toEntry.ID)
-		require.NoError(t, err)
-
-		// check accounts
-		fromAccount := result.FromAccount
-		require.NotEmpty(t, fromAccount)
-		require.Equal(t, acc1.ID, fromAccount.ID)
-
-		toAccount := result.ToAccount
-		require.NotEmpty(t, toAccount)
-		require.Equal(t, acc2.ID, toAccount.ID)
-
-		// check accounts balance
-		fmt.Println(">> tx: ", fromAccount.Balance, toAccount.Balance)
-		diff1 := acc1.Balance - fromAccount.Balance
-		diff2 := toAccount.Balance - acc2.Balance
-		require.Equal(t, diff1, diff2)
-		require.True(t, diff1 > 0)
-		require.True(t, diff1%amount == 0)
-
-		k := int(diff1 / amount)
-		require.True(t, k >= 1 && k <= n)
-		require.NotContains(t, existed, k)
-		existed[k] = true
 	}
 
 	// check the final updated balances
@@ -215,6 +160,6 @@ func TestTransferTxDeadlock(t *testing.T) {
 
 	fmt.Println(">> After: ", acc1.Balance, acc2.Balance)
 
-	require.Equal(t, acc1.Balance-int64(n)*amount, updateAccount1.Balance)
-	require.Equal(t, acc2.Balance+int64(n)*amount, updateAccount2.Balance)
+	require.Equal(t, acc1.Balance, updateAccount1.Balance)
+	require.Equal(t, acc2.Balance, updateAccount2.Balance)
 }
